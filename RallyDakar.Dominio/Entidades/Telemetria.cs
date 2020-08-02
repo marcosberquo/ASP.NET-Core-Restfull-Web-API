2bf9a042da9d4ef549aa5562c3649076e27a40e1
marcosberquo <marcosberquo@hotmail.com>
2020-08-02 07:05:15 -0300

    Add project files.

---
+++ RallyDakar.Dominio/Entidades/Telemetria.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RallyDakar.Dominio.Entidades
+{
+    public class Telemetria
+    {
+        public int Id { get; set; }
+        public int TemporadaId { get; set; }
+        public int PilotoId { get; set; }
+
+        public DateTime Date { get; set; }
+        public TimeSpan Hora { get; set; }
+
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal PercentualCombustivel { get; set; }
+        public decimal Velocidade { get; set; }
+        public double RMP { get; set; }
+        public int TemperaturaExerna { get; set; }
+        public int TemperaturaMotor { get; set; }
+        public double AltitudeNivelMar { get; set; }
+
+        public bool PedalAcelerador { get; set; }
+        public bool PedalFreio { get; set; }
+
+    }
+}
---
+++ RallyDakar.Dominio/Entidades/Temporada.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RallyDakar.Dominio.Entidades
+{
+    public class Temporada
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime? DataFim { get; set; }
+
+        public ICollection<Equipe> Esquipes { get; set; }
+
+
+    }
+}
